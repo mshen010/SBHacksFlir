3060bf9f1f7cea02efa75ea730c6261a28744bfb
dzhan008 <dzhan008@ucr.edu>
2016-04-23 09:19:20 -0700

    Updated compiler version

---
+++ FLIRONEExampleApplication/app/src/main/java/com/flir/flironeexampleapplication/Patient.java
@@ -0,0 +1,24 @@
+package com.flir.flironeexampleapplication;
+
+/**
+ * Created by skill on 4/23/2016.
+ */
+public class Patient {
+    private String name;
+    private int age;
+
+    public Patient() {
+
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+}
